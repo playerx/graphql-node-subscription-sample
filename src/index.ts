57fb696a7245dd2e4e20a2fc39928231b80e3e39
Ezeki Zibzibadze <ez@jok.io>
2017-10-13 00:48:14 +0400

    created simple graphql app with subscription support

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import { createServer } from 'http';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import { SubscriptionServer } from 'subscriptions-transport-ws';
+import { execute, subscribe } from 'graphql';
+
+import { schema } from './schema';
+
+const myGraphQLSchema = schema;// ... define or import your schema here!
+const PORT = 3000;
+
+const app = express();
+
+// bodyParser is needed just for POST.
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: myGraphQLSchema }));
+app.get('/graphiql', graphiqlExpress({
+    endpointURL: '/graphql',
+    subscriptionsEndpoint: `ws://localhost:${PORT}/subscriptions`
+})); // if you want GraphiQL enabled
+
+const ws = createServer(app);
+
+ws.listen(PORT, () => {
+    console.log(`GraphQL Server is now running on http://localhost:${PORT}`);
+
+    // Set up the WebSocket for handling GraphQL subscriptions
+    new SubscriptionServer({
+        execute,
+        subscribe,
+        schema
+    }, { server: ws, path: '/subscriptions', });
+});
+
---
+++ src/resolvers.ts
@@ -0,0 +1,100 @@
+import { PubSub } from 'graphql-subscriptions';
+import { withFilter } from 'graphql-subscriptions';
+import * as faker from 'faker';
+
+const channels: any[] = [];
+let lastChannelId = 0;
+let lastMessageId = 0;
+let messageCreatedAt = 123456789;
+
+function addChannel(name: any) {
+    lastChannelId++;
+    const newChannel = {
+        id: String(lastChannelId),
+        name: name,
+        messages: [],
+    };
+    channels.push(newChannel);
+    return lastChannelId;
+}
+
+function getChannel(id: any) {
+    return channels.filter(channel => channel.id === id)[0];
+}
+
+function addFakeMessage(channel: any, messageText: any) {
+    lastMessageId++;
+    messageCreatedAt++;
+    const newMessage = {
+        id: lastMessageId,
+        createdAt: messageCreatedAt,
+        text: messageText,
+    };
+    channel.messages.push(newMessage);
+}
+
+// use faker to generate random messages in faker channel
+addChannel('faker');
+const fakerChannel = channels.filter(channel => channel.name === 'faker')[0];
+
+// Add seed for consistent random data
+faker.seed(9);
+for (let i = 0; i < 50; i++) {
+    addFakeMessage(fakerChannel, faker.random.words());
+}
+
+// generate second channel for initial channel list view
+addChannel('channel2');
+
+const pubsub = new PubSub();
+
+export const resolvers = {
+    Query: {
+        channels: () => {
+            return channels;
+        },
+
+        channel: (root: any, { id }: any) => {
+            return getChannel(id);
+        },
+    },
+    Mutation: {
+        addChannel: (root: any, args: any) => {
+            const name = args.name;
+            const id = addChannel(name);
+            return getChannel(id);
+        },
+        addMessage: (root: any, { message }: any) => {
+            const channel = channels.filter(
+                channel => channel.id === message.channelId
+            )[0];
+            if (!channel) throw new Error('Channel does not exist');
+
+            const newMessage = {
+                id: String(lastMessageId++),
+                text: message.text,
+                createdAt: +new Date(),
+            };
+            channel.messages.push(newMessage);
+
+            pubsub.publish('messageAdded', {
+                messageAdded: newMessage,
+                channelId: message.channelId,
+            });
+
+            return newMessage;
+        },
+    },
+    Subscription: {
+        messageAdded: {
+            subscribe: withFilter(
+                () => pubsub.asyncIterator('messageAdded'),
+                (payload, variables) => {
+                    // The `messageAdded` channel includes events for all channels, so we filter to only
+                    // pass through events for the channel specified in the query
+                    return payload.channelId === variables.channelId;
+                }
+            ),
+        },
+    },
+};
